48ed5f55cb3432d8708a06d2d68b68804303918c
kvillamil1 <kvillamil16@my.whitworth.edu>
2015-11-02 21:06:35 -0800

    New GUI
    New GUI

---
+++ src/pkginterface/Interface.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkginterface;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Kat
+ */
+
+public class Interface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
+
